65504428709b71d3c33c9e12835c02eebc0d0c24
VaiNaWeb <johndoe@example.com>
2019-07-12 15:07:01 -0300

    consertando

--- main.js
+++ main.js
@@ -1,7 +1,7 @@
 let numero = 0;
 //let janela = document.getElementByClassName('corpo-do-site')
 
-let openClose = (click) => {
+let openClose = () => {
 	
 	let parouinpar = numero % 2;
 	if (parouinpar == 0 ){
@@ -9,7 +9,7 @@ let openClose = (click) => {
 		linea3.style.opacity = 0;
 		linea1.style.transition = 'all 0.3s ease-in-out'
 		linea1.style.opacity = 0;
-	 	//menuMobile.style.display = 'block';
+	 	menuMobile.style.display = 'block';
 	 	numero++
 	}
 	else {
@@ -17,7 +17,7 @@ let openClose = (click) => {
 		linea3.style.opacity = 1;
 		linea1.style.transition = ''
 		linea1.style.opacity = 1;
-		//menuMobile.style.display = 'none';
+		menuMobile.style.display = 'none';
 		numero++
 	}
 	
